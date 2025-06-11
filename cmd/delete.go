73e7208c0f39b4629f6c4d9b5e8d4817e13555fc
Adithyan A <adithyanthedev@gmail.com>
2025-06-11 10:41:51 +0530

    docs: Add proper usage for `delete` command

--- cmd/delete.go
+++ cmd/delete.go
@@ -10,6 +10,10 @@ func deleteCommand() *cli.Command {
 	return &cli.Command{
 		Name:  "delete",
 		Usage: "Delete an existing script",
+		UsageText: "" +
+			"yasm delete                  Open a menu to select and delete a script.\n" +
+			"yasm delete <script-name>    Delete the script with given name directy.",
+
 		Action: func(c *cli.Context) error {
 			scriptName := c.Args().First()
 			return scripts.DeleteScript(scriptName)

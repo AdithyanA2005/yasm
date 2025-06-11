b5a50592cd3188d82436cdc3c5e6def008c29ba2
Adithyan A <adithyanthedev@gmail.com>
2025-06-11 13:38:50 +0530

    docs: Add more comments to `cmd/edit.go`

--- cmd/edit.go
+++ cmd/edit.go
@@ -6,6 +6,9 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+// editCommand returns a new CLI command for editing scripts.
+// This command allows the user to edit an existing script by name,
+// or open a menu to select and edit a script if no name is provided.
 func editCommand() *cli.Command {
 	return &cli.Command{
 		Name:  "edit",

244297c3c2ee8d83c2734707f0b0481730fcddd9
Adithyan A <adithyanthedev@gmail.com>
2025-06-11 14:54:37 +0530

    chore: Remote unwanted comment block from `scripts/languages.go`

--- scripts/languages.go
+++ scripts/languages.go
@@ -67,15 +67,3 @@ func PrintLanguagesTable() {
 
 	table.Render()
 }
-
-// Supported shebangs by language
-// var shebangs = map[string]string{
-// 	"bash":   "#!/usr/bin/env bash",
-// 	"python": "#!/usr/bin/env python3",
-// 	"sh":     "#!/bin/sh",
-// 	"zsh":    "#!/usr/bin/env zsh",
-// }
-//
-// func ListSupportedLanguages() map[string]string {
-// 	return shebangs
-// }

61822bcb55f470b30589c28d2ae51ce7c2357bb5
Adithyan A <adithyanthedev@gmail.com>
2025-06-11 10:48:28 +0530

    docs: Add proper usage for `edit` command

--- cmd/edit.go
+++ cmd/edit.go
@@ -10,6 +10,10 @@ func editCommand() *cli.Command {
 	return &cli.Command{
 		Name:  "edit",
 		Usage: "Edit an existing script",
+		UsageText: "" +
+			"yasm edit                  Open a menu to select a script and edit it in editor.\n" +
+			"yasm edit <script-name>    Edit the script with given name directy in editor.",
+
 		Action: func(c *cli.Context) error {
 			scriptName := c.Args().First()
 			return scripts.EditScript(scriptName)

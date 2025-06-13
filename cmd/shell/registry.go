27df7166fc5ef8aa9032f35cd8d93cb59278c724
Adithyan A <adithyanthedev@gmail.com>
2025-06-13 19:41:35 +0530

    chore: Add more docs to shell.registry

--- cmd/shell/registry.go
+++ cmd/shell/registry.go
@@ -5,9 +5,12 @@ type shellDefinition struct {
 	Script string
 }
 
+// SupportedShells is a map of supported shell names to their embeded integration scripts.
 var SupportedShells = map[string]shellDefinition{
 	"bash": {Name: "bash", Script: bashScript},
 	"zsh":  {Name: "zsh", Script: zshScript},
 	"fish": {Name: "fish", Script: fishScript},
 }
+
+// Special prefix that is used with commands to inject them in user's shell.
 const InjectPrefix = "::yasm-inject::"
